5f396d83ce517945f463d58fbe7d780c899ef1c6
Mehadi Shisir <mehadishisir@gmail.com>
2026-09-16 03:16:39 +0600

    feat: implement LoginForm with form handling and UI components for improved user experience feat: add reusable Field, Input, Label, and Separator components for consistent UI structure

--- src/components/form/login-form.tsx
+++ src/components/form/login-form.tsx
@@ -1,7 +1,55 @@
+"use client"
+
+
+import { useForm } from '@tanstack/react-form'
 import React from 'react'
+import { Button } from '../ui/button'
+import { Input } from '../ui/input'
+import { Field, FieldLabel } from '../ui/field'
+import { Autocomplete } from '@base-ui/react'
 
 export default function LoginForm() {
+  const form = useForm({
+    defaultValues:{
+      email:"",
+      password:""
+    },
+    onSubmit:({value})=>{
+      console.log(value)
+    }
+  })
   return (
-    <div>LoginForm</div>
+    <div>
+      <p>Login Form</p>
+      <form 
+      onSubmit={(e)=>{
+        e.preventDefault()
+        form.handleSubmit()
+      }}>
+        <form.Field name="email">
+          {(field)=>{
+         return (
+          <Field>
+            <FieldLabel htmlFor={field.name}>
+            Email
+          </FieldLabel>
+          <Input  
+          id={field.name}
+          name={field.name} 
+          onChange={(e)=>field.handleChange(e.target.value)
+            
+          }
+          value={field.state.value}
+          onBlur={field.handleBlur}
+
+          autoComplete='off'
+          ></Input>
+          </Field>
+        )
+        }
+          }</form.Field>
+          <Button type='submit'>Submit</Button>
+      </form>
+    </div>
   )
 }
